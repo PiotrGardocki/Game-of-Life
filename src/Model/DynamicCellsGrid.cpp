2dd55549cb46735bc6b9914fd03ebed9d26fb72a
Piotr Gardocki <pgardocki@interia.pl>
2019-08-23 20:43:35 +0200

    Add size adjusting in DynamicCellsGrid

--- src/Model/DynamicCellsGrid.cpp
+++ src/Model/DynamicCellsGrid.cpp
@@ -15,23 +15,36 @@ std::vector<bool>::const_reference DynamicCellsGrid::getCell(size_t column, size
 	return cells.at(column).at(row);
 }
 
-void DynamicCellsGrid::setCell(size_t column, size_t row, bool alive)
+void DynamicCellsGrid::setCell(int column, int row, bool alive)
 {
-	auto columnsNum = getColumnsNum();
-	auto rowsNum = getRowsNum();
+	auto columnsNum = static_cast<int>(getColumnsNum());
+	auto rowsNum = static_cast<int>(getRowsNum());
 
-	if (column < columnsNum && row < rowsNum)
+	if (column < columnsNum && column >= 0 && row < rowsNum && row >= 0)
 	{
+		getCell(column, row) = alive;
+	}
+	else if (alive)
+	{
+		if (column < 0)
+			appendColumnsAtLeft(-column);
+		if (row < 0)
+			appendRowsAtTop(-row);
+		if (column >= columnsNum)
+			appendColumnsAtRight(column - columnsNum + 1);
+		if (row >= rowsNum)
+			appendRowsAtBottom(row - rowsNum + 1);
+
 		getCell(column, row) = alive;
 	}
 }
 
-void DynamicCellsGrid::turnCellOn(size_t column, size_t row)
+void DynamicCellsGrid::turnCellOn(int column, int row)
 {
 	setCell(column, row, true);
 }
 
-void DynamicCellsGrid::turnCellOff(size_t column, size_t row)
+void DynamicCellsGrid::turnCellOff(int column, int row)
 {
 	setCell(column, row, false);
 }
@@ -44,4 +57,35 @@ size_t DynamicCellsGrid::getColumnsNum() const
 size_t DynamicCellsGrid::getRowsNum() const
 {
 	return cells.size() ? cells.at(0).size() : 0;
+}
+
+void DynamicCellsGrid::appendRowsAtTop(size_t num)
+{
+	auto columnsNum = getColumnsNum();
+	for (size_t column = 0; column < columnsNum; ++column)
+		for (size_t i = 0; i < num; ++i)
+		{
+			auto& col = cells.at(column);
+			col.insert(col.cbegin(), false);
+		}
+}
+
+void DynamicCellsGrid::appendRowsAtBottom(size_t num)
+{
+	auto columnsNum = getColumnsNum();
+	for (size_t column = 0; column < columnsNum; ++column)
+		for (size_t i = 0; i < num; ++i)
+			cells.at(column).push_back(false);
+}
+
+void DynamicCellsGrid::appendColumnsAtLeft(size_t num)
+{
+	for (size_t i = 0; i < num; ++i)
+		cells.emplace(cells.cbegin(), getRowsNum(), false);
+}
+
+void DynamicCellsGrid::appendColumnsAtRight(size_t num)
+{
+	for (size_t i = 0; i < num; ++i)
+	cells.emplace_back(getRowsNum(), false);
 }
--- src/Model/DynamicCellsGrid.hpp
+++ src/Model/DynamicCellsGrid.hpp
@@ -11,12 +11,18 @@ public:
 	virtual std::vector<bool>::reference getCell(size_t column, size_t row);
 	virtual std::vector<bool>::const_reference getCell(size_t column, size_t row) const;
 
-	void setCell(size_t column, size_t row, bool alive);
-	void turnCellOn(size_t column, size_t row);
-	void turnCellOff(size_t column, size_t row);
+	void setCell(int column, int row, bool alive);
+	void turnCellOn(int column, int row);
+	void turnCellOff(int column, int row);
 
 	size_t getColumnsNum() const;
 	size_t getRowsNum() const;
+
+private:
+	void appendRowsAtTop(size_t num);
+	void appendRowsAtBottom(size_t num);
+	void appendColumnsAtLeft(size_t num);
+	void appendColumnsAtRight(size_t num);
 
 private:
 	std::vector<std::vector<bool>> cells;  // columns<rows>
