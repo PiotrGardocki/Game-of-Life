54c4cf39789b6b27721f97f7332956b8019a0e85
Piotr Gardocki <pgardocki@interia.pl>
2019-08-24 23:41:38 +0200

    Add missing 'override' keyword

--- src/Model/DynamicCellsGrid.hpp
+++ src/Model/DynamicCellsGrid.hpp
@@ -8,15 +8,15 @@ public:
 	DynamicCellsGrid() = default;
 	DynamicCellsGrid(size_t columns, size_t rows);
 
-	virtual std::vector<bool>::reference getCell(size_t column, size_t row);
-	virtual std::vector<bool>::const_reference getCell(size_t column, size_t row) const;
+	std::vector<bool>::reference getCell(size_t column, size_t row) override;
+	std::vector<bool>::const_reference getCell(size_t column, size_t row) const override;
 
 	void setCell(int column, int row, bool alive);
 	void turnCellOn(int column, int row);
 	void turnCellOff(int column, int row);
 
-	size_t getColumnsNum() const;
-	size_t getRowsNum() const;
+	size_t getColumnsNum() const override;
+	size_t getRowsNum() const override;
 
 private:
 	void appendRowsAtTop(size_t num);
