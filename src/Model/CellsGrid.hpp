a251296d6d8b4652d3967a75bea3fc835366ebde
Piotr Gardocki <pgardocki@interia.pl>
2019-08-24 23:41:09 +0200

    Add two methods to interface

--- src/Model/CellsGrid.hpp
+++ src/Model/CellsGrid.hpp
@@ -7,4 +7,7 @@ class CellsGrid
 public:
 	virtual std::vector<bool>::reference getCell(size_t column, size_t row) = 0;
 	virtual std::vector<bool>::const_reference getCell(size_t column, size_t row) const = 0;
+
+	virtual size_t getColumnsNum() const = 0;
+	virtual size_t getRowsNum() const = 0;
 };
