1cd59836a3961c166fd19e610107273d716ed2b9
Piotr Gardocki <pgardocki@interia.pl>
2019-08-31 23:24:45 +0200

    Add button to update cells

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,10 +2,24 @@
 
 #include <View/GridWidget.hpp>
 
+#include <QVBoxLayout>
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 	, grid(7, 5)
 {
-	GridWidget* widget = new GridWidget(grid);
-	setCentralWidget(widget);
+	QWidget* centralWidget = new QWidget(this);
+	setCentralWidget(centralWidget);
+
+	QVBoxLayout* layout = new QVBoxLayout;
+	centralWidget->setLayout(layout);
+
+	GridWidget* gridWidget = new GridWidget(grid);
+	layout->addWidget(gridWidget);
+
+	QPushButton* button = new QPushButton("Update");
+	layout->addWidget(button);
+
+	connect(button, &QPushButton::clicked, gridWidget, &GridWidget::updateOneStep);
 }
